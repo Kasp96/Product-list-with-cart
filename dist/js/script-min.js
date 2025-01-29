f5d2792e3280fbaf6750c7982c16d4ee704fb441
Kacper <kacperwach96@gmail.com>
2025-01-29 15:07:08 +0100

    repo init

---
+++ dist/js/script-min.js
@@ -0,0 +1,2 @@
+console.log("test");
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInNjcmlwdC5qcyJdLCJuYW1lcyI6WyJjb25zb2xlIiwibG9nIl0sIm1hcHBpbmdzIjoiQUFBQUEsUUFBUUMsSUFBSSxNQUFNIiwiZmlsZSI6InNjcmlwdC1taW4uanMiLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygndGVzdCcpOyJdfQ==
